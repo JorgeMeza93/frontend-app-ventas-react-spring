6867cfd2d46a5a8ce39c3e876c8e6d7d68f50380
Jorge Meza Bello <jorberiver@gmail.com>
2022-09-30 20:22:49 -0500

    routes añadidas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "bulma/css/bulma.css";
 import "@fortawesome/fontawesome-free/js/all"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import Home from "./components/pages/Home";
+import Clientes from "./components/pages/Clientes";
 
 function App() {
   return (
     <div className="App">
-      <Layout/>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/clientes" element={<Clientes/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,8 +5,17 @@ import Menu from './Menu';
 const Layout = () => {
   return (
     <div>
-        <Header />
-        <Menu />
+        <Header/>
+        <div className="container">
+            <div className="columns">
+                <div className="column is-one-quarter">
+                    <Menu/>
+                </div>
+                <div className='column'>
+                    <p>Este es el resto del contenido</p>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
