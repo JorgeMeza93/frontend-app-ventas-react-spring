ea8f00a76de151e3208bf509a8e4073d8589ccb3
Jorge Meza Bello <jorberiver@gmail.com>
2022-09-30 22:50:27 -0500

    Tabla clientes  modificada

--- src/components/pages/Clientes.jsx
+++ src/components/pages/Clientes.jsx
@@ -5,7 +5,38 @@ const Clientes = () => {
   return (
     <div>
         <Layout>
-            <p>Clientes</p>
+            <div className="panel-heading">
+                Clientes
+            </div>
+            <div className="box">
+                <table className='table is-hoverable'>
+                    <thead>
+                        <tr>
+                            <th>Acciones</th>
+                            <th>Nombres</th>
+                            <th>Apellidos</th>
+                            <th>Dirección</th>
+                            <th>Teléfono</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr><td>
+                                <button className='button'>
+                                    <span className='icon is-email'>
+                                        <i className='fas fa-bold'></i>
+                                    </span>
+                                </button>
+                            </td>
+                            <td>Jorge</td>
+                            <td>Meza</td>
+                            <td>Calle Samuel Goldstein 546, col Verde Valle </td>
+                            <td>2222209901</td>
+                            <td>jorberiverplate@yahoo.com.mx</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </Layout>
     </div>
   )
