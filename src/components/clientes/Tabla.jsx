e11ad3a01e50b3816c1267b79f0dc33c8a2cc2b1
Jorge Meza Bello <jorberiver@gmail.com>
2022-10-01 23:49:17 -0500

    state de la tabla modificada

--- src/components/clientes/Tabla.jsx
+++ src/components/clientes/Tabla.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Tabla = () => {
+  const [clientesList, setClientesList] = useState([
+    {
+        "idCliente": 1,
+        "nombre": "Jorge",
+        "apellido": "Meza",
+        "direccion": "Calle Samuel Goldstein 546, col Verde Valle",
+        "telefono": "2225169936",
+        "email": "jorge-riverplate.com"
+    },
+    {
+        "idCliente": 2,
+        "nombre": "James",
+        "apellido": "McGill",
+        "direccion": "Calle Broadway 234, Saint Michael",
+        "telefono": "2220200181",
+        "email": "jimmy-mcgill@yahoo.com"
+    }
+  ]);
+
   return (
     <div className='table-container'>
         <table className='table is-hoverable'>
             <thead>
                 <tr>
-                    <th>Acciones</th>
+                    <th>Acciones </th>
                     <th>Nombres</th>
                     <th>Apellidos</th>
                     <th>Dirección</th>
@@ -15,25 +34,32 @@ const Tabla = () => {
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>
-                        <button className='button is-small is-info mr-1' title='Modificar'>
-                            <span className='icon is-email'>
-                                <i className='fas fa-edit'></i>
-                            </span>
-                        </button>
-                        <button className='button is-small is-danger' title='Eliminar'>
-                            <span className='icon is-email'>
-                                <i className='fas fa-trash-alt'></i>
-                            </span>
-                        </button>
-                    </td>
-                    <td>Jorge</td>
-                    <td>Meza</td>
-                    <td>Calle Samuel Goldstein 546, col Verde Valle </td>
-                    <td>2222209901</td>
-                    <td>jorberiverplate@yahoo.com.mx</td>
-                </tr>
+                {
+                    clientesList.map( cliente => {
+                        const {nombre, apellidos, direccion, telefono, email} = cliente;
+                        return (
+                        <tr>
+                        <td>
+                            <button className='button is-small is-info mr-1' title='Modificar'>
+                                <span className='icon is-email'>
+                                    <i className='fas fa-edit'></i>
+                                </span>
+                            </button>
+                            <button className='button is-small is-danger' title='Eliminar'>
+                                <span className='icon is-email'>
+                                    <i className='fas fa-trash-alt'></i>
+                                </span>
+                            </button>
+                        </td>
+                        <td>{nombre}</td>
+                        <td>{apellidos}</td>
+                        <td>{direccion}</td>
+                        <td>{telefono}</td>
+                        <td>{email}</td>
+                    </tr>)
+                    })
+                }
+               
             </tbody>
         </table>
     </div>
@@ -41,3 +67,5 @@ const Tabla = () => {
 }
 
 export default Tabla
+
+//Calle Samuel Goldstein 546, col Verde Valle 
